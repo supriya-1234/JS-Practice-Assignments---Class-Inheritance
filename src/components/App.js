e2efeeebb660ef4ccd6a4d4cc531176185fd8be7
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-31 23:24:06 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 import Circle from "./circle";
-
+import Shape from "./shape";
 class App extends Component {
   render() {
     return (
       <>
         <Circle />
+        <Shape />
       </>
     );
   }
